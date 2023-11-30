1d62f3cba3aa976d34d01b16d9f4d3c8657ab4aa
Simon Verhoeven <simon.verhoeven>
2023-12-01 03:01:56 +0800

    Next Auth Setup

--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import { CreatePost } from "~/app/_components/create-post";
-import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
-import styles from "./index.module.css";
+import { CreatePost } from '~/app/_components/create-post';
+import { getServerAuthSession } from '~/server/auth';
+import { api } from '~/trpc/server';
+import styles from './index.module.css';
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const hello = await api.post.hello.query({ text: 'from tRPC' });
   const session = await getServerAuthSession();
 
   return (
@@ -16,43 +16,22 @@ export default async function Home() {
           Create <span className={styles.pinkSpan}>T3</span> App
         </h1>
         <div className={styles.cardRow}>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
+          <Link className={styles.card} href='https://create.t3.gg/en/usage/first-steps' target='_blank'>
             <h3 className={styles.cardTitle}>First Steps →</h3>
-            <div className={styles.cardText}>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
+            <div className={styles.cardText}>Just the basics - Everything you need to know to set up your database and authentication.</div>
           </Link>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
+          <Link className={styles.card} href='https://create.t3.gg/en/introduction' target='_blank'>
             <h3 className={styles.cardTitle}>Documentation →</h3>
-            <div className={styles.cardText}>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
+            <div className={styles.cardText}>Learn more about Create T3 App, the libraries it uses, and how to deploy it.</div>
           </Link>
         </div>
         <div className={styles.showcaseContainer}>
-          <p className={styles.showcaseText}>
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
+          <p className={styles.showcaseText}>{hello ? hello.greeting : 'Loading tRPC query...'}</p>
 
           <div className={styles.authContainer}>
-            <p className={styles.showcaseText}>
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className={styles.loginButton}
-            >
-              {session ? "Sign out" : "Sign in"}
+            <p className={styles.showcaseText}>{session && <span>Logged in as {session.user?.email}</span>}</p>
+            <Link href={session ? '/api/auth/signout' : '/api/auth/signin'} className={styles.loginButton}>
+              {session ? 'Sign out' : 'Sign in'}
             </Link>
           </div>
         </div>
@@ -72,9 +51,7 @@ async function CrudShowcase() {
   return (
     <div className={styles.showcaseContainer}>
       {latestPost ? (
-        <p className={styles.showcaseText}>
-          Your most recent post: {latestPost.name}
-        </p>
+        <p className={styles.showcaseText}>Your most recent post: {latestPost.name}</p>
       ) : (
         <p className={styles.showcaseText}>You have no posts yet.</p>
       )}
--- apps/next-app/src/env.js
+++ apps/next-app/src/env.js
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
   /**
@@ -7,13 +7,8 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    NEXTAUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string() : z.string().optional(),
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
@@ -22,8 +17,9 @@ export const env = createEnv({
       process.env.VERCEL ? z.string() : z.string().url()
     ),
     // Add ` on ID and SECRET if you want to make sure they're not empty
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+    COGNITO_CLIENT_ID: z.string(),
+    COGNITO_CLIENT_SECRET: z.string(),
+    COGNITO_ISSUER: z.string(),
   },
 
   /**
@@ -43,8 +39,9 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_CLIENT_SECRET: process.env.COGNITO_CLIENT_SECRET,
+    COGNITO_ISSUER: process.env.COGNITO_ISSUER,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
