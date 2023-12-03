e9cae8abe8ce34ca357f3a7db08b4a57821f184a
Simon Verhoeven <simon.verhoeven>
2023-12-04 03:45:07 +0800

    debug issue

--- apps/next-app/src/trpc/react.tsx
+++ apps/next-app/src/trpc/react.tsx
@@ -1,17 +1,18 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
-import { createTRPCReact } from "@trpc/react-query";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
+import { createTRPCReact } from '@trpc/react-query';
+import { useState } from 'react';
 
-import { type AppRouter } from "~/server/api/root";
-import { getUrl, transformer } from "./shared";
+import { type AppRouter } from '~/server/api/root';
+import { getUrl, transformer } from './shared';
 
 export const api = createTRPCReact<AppRouter>();
 
 export function TRPCReactProvider(props: {
   children: React.ReactNode;
+  // eslint-disable-next-line react/no-unused-prop-types
   cookies: string;
 }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -21,19 +22,17 @@ export function TRPCReactProvider(props: {
       transformer,
       links: [
         loggerLink({
-          enabled: (op) =>
-            process.env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
+          enabled: (op) => process.env.NODE_ENV === 'development' || (op.direction === 'down' && op.result instanceof Error),
         }),
-        unstable_httpBatchStreamLink({
-          url: getUrl(),
-          headers() {
-            return {
-              cookie: props.cookies,
-              "x-trpc-source": "react",
-            };
-          },
-        }),
+        // unstable_httpBatchStreamLink({
+        //   url: getUrl(),
+        //   headers() {
+        //     return {
+        //       cookie: props.cookies,
+        //       "x-trpc-source": "react",
+        //     };
+        //   },
+        // }),
       ],
     })
   );
