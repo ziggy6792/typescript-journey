0d8438c1b8ec4db6d65342edb857c33ca22ce8f0
Simon Verhoeven <simon.verhoeven>
2023-12-04 18:59:07 +0800

    app router working

--- apps/sst-app/sst.config.ts
+++ apps/sst-app/sst.config.ts
@@ -12,7 +12,7 @@ export default {
     return {
       name: commonConfig.PROJECT_NAME,
       region: 'ap-southeast-1',
-      stage: 'foobar',
+      stage: 'example',
     };
   },
   stacks(app) {
--- apps/sst-app/stacks/NextApp.ts
+++ apps/sst-app/stacks/NextApp.ts
@@ -8,14 +8,14 @@ export function NextApp({ stack }: StackContext) {
   // const { userPoolClientId, userPoolClientSecret } = auth.cdk.userPoolClient;
 
   const site = new NextjsSite(stack, 'next-app', {
-    path: path.join(require.resolve('@ts-journey/next-app'), '..'),
+    path: path.join(require.resolve('@ts-journey/app-router'), '..'),
     buildCommand: 'yarn open:next:build',
     bind: [],
-    environment: {
-      COGNITO_CLIENT_ID: 'XXXXXXXXXXXXXXXXXXXXXXXXX',
-      COGNITO_CLIENT_SECRET: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-      COGNITO_ISSUER: `https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_XXXXXXXXX`,
-    },
+    // environment: {
+    //   COGNITO_CLIENT_ID: 'XXXXXXXXXXXXXXXXXXXXXXXXX',
+    //   COGNITO_CLIENT_SECRET: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    //   COGNITO_ISSUER: `https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_XXXXXXXXX`,
+    // },
     // experimental: {
     //   streaming: true,
     // },
