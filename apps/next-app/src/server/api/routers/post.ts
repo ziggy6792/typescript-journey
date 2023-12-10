2e4dbdcfc518f6d171e88a68a848d27e23a9f6e7
Simon Verhoeven <simon.verhoeven>
2023-12-11 00:40:35 +0800

    s3 file upload

--- apps/next-app/src/server/api/routers/post.ts
+++ apps/next-app/src/server/api/routers/post.ts
@@ -1,34 +1,40 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { commonUtils } from '@ts-journey/common';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '~/server/api/trpc';
 
 let post = {
   id: 1,
-  name: "Hello World",
+  name: 'Hello World',
 };
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => ({
-        greeting: `Hello ${input.text}`,
-      })),
+  hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => ({
+    greeting: `Hello ${input.text}`,
+  })),
 
-  create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+  create: protectedProcedure.input(z.object({ name: z.string().min(1) })).mutation(async ({ input }) => {
+    // simulate a slow db call
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      post = { id: post.id + 1, name: input.name };
-      return post;
-    }),
+    post = { id: post.id + 1, name: input.name };
+    return post;
+  }),
 
   getLatest: protectedProcedure.query(() => post),
 
-  getSecretMessage: protectedProcedure.query(() => "you can now see this secret message!"),
+  getSecretMessage: protectedProcedure.query(() => 'you can now see this secret message!'),
+
+  getUploadUrl: protectedProcedure.mutation(async () => {
+    console.log('process.env.SST_STAGE', process.env.SST_STAGE);
+    const command = new PutObjectCommand({
+      ACL: 'public-read',
+      Key: crypto.randomUUID(),
+      Bucket: commonUtils.getS3BucketName(commonUtils.BucketName.FILE_UPLOADS, process.env.SST_STAGE!),
+    });
+    const url = (await getSignedUrl(new S3Client({}), command)) as string;
+    return url;
+  }),
 });
