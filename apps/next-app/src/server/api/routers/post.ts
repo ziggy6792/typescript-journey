9c2969991b01505edc9f19e8c25f95b7cbb1a847
Simon Verhoeven <simon.verhoeven>
2023-12-12 01:47:17 +0800

    use sst bind

--- apps/next-app/src/server/api/routers/post.ts
+++ apps/next-app/src/server/api/routers/post.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { commonUtils } from '@ts-journey/common';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { Bucket } from 'sst/node/bucket';
 import { createTRPCRouter, protectedProcedure, publicProcedure } from '~/server/api/trpc';
 
 let post = {
@@ -32,7 +31,7 @@ export const postRouter = createTRPCRouter({
     const command = new PutObjectCommand({
       ACL: 'public-read',
       Key: crypto.randomUUID(),
-      Bucket: Bucket['file-uploads'].bucketName,
+      Bucket: commonUtils.getS3BucketName(commonUtils.BucketName.FILE_UPLOADS, process.env.SST_STAGE!),
     });
     const url = (await getSignedUrl(new S3Client({}), command)) as string;
     return url;
