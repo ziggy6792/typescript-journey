8c95441d1f0cfec5867704b5a82cfcd90d90cae8
Simon Verhoeven <simon.verhoeven>
2023-12-12 01:22:08 +0800

    fix utility

--- apps/sst-app/utils/utility.ts
+++ apps/sst-app/utils/utility.ts
@@ -1,3 +1,3 @@
-import { App } from 'sst/constructs/App';
+import { App } from 'sst/constructs';
 
 export const getConstructName = (name: string, app: App): string => [app.name, app.stage, name].join('-');
