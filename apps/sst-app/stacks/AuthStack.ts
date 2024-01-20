075d1557991287e8aa01b69823ea4cfd1d425c03
Simon Verhoeven <simon.verhoeven>
2024-01-20 17:01:19 +0800

    production config (remember to run build and deploy again)

--- apps/sst-app/stacks/AuthStack.ts
+++ apps/sst-app/stacks/AuthStack.ts
@@ -1,8 +1,28 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Cognito, StackContext } from 'sst/constructs';
 import { getConstructName } from '../utils/utility';
 
+// Workaround to get stack output for previous sst deplyoment, if it exists
+const getStackOutputs = () => {
+  try {
+    return require('../.sst/outputs.json');
+  } catch (error) {
+    console.warn('No previous deployment found, pleease deploy again after this deployment is complete');
+    return {};
+  }
+};
+
+const stackOutputs = getStackOutputs();
+
 export function AuthStack({ stack, app }: StackContext) {
-  const callbackUrls = ['http://localhost:3000/api/auth/callback/cognito', 'https://REPLACE_ME.cloudfront.net/api/auth/callback/cognito'];
+  const callbackUrls = ['http://localhost:3000/api/auth/callback/cognito'];
+
+  const productionCloudfrontUrl = stackOutputs[`${app.stage}-${app.name}-NextApp`]?.url;
+
+  if (productionCloudfrontUrl) {
+    callbackUrls.push(`${productionCloudfrontUrl}/api/auth/callback/cognito`);
+  }
 
   const auth = new Cognito(stack, 'Auth', {
     login: ['email'],
