7a7ec2b659efa0d3d83b91f761d31288e2905e10
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-07-21 19:19:28 +0700

    docker provider demo

--- apps/cdktf-app/main.ts
+++ apps/cdktf-app/main.ts
@@ -1,11 +1,34 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import { container, image, provider } from '@cdktf/provider-docker';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    // Add Docker provider
+    new provider.DockerProvider(this, 'docker', {
+      // Not sure why default doesnt work, got this from docker context ls
+      host: 'unix:///Users/simon.verhoeven/.docker/run/docker.sock',
+    });
+
     // define resources here
+
+    const myImage = new image.Image(this, 'image', {
+      name: 'nginx:latest',
+      keepLocally: false,
+    });
+
+    const myContainer = new container.Container(this, 'nginxContainer', {
+      image: myImage.name,
+      name: 'demo',
+      ports: [
+        {
+          internal: 80,
+          external: 8000,
+        },
+      ],
+    });
   }
 }
 
