21170ac6e77d2da11ecda7ac88d9858416959fb4
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-07-22 00:35:10 +0700

    try with asset

--- apps/cdktf-app/main.ts
+++ apps/cdktf-app/main.ts
@@ -1,8 +1,9 @@
 import { Construct } from 'constructs';
-import { App, S3Backend, TerraformStack } from 'cdktf';
-import { provider } from '@cdktf/provider-aws';
-import { S3Bucket } from '@cdktf/provider-aws/lib/s3-bucket';
-import { S3BucketObject } from '@cdktf/provider-aws/lib/s3-bucket-object';
+import { App, AssetType, S3Backend, TerraformAsset, TerraformStack } from 'cdktf';
+import { provider, s3BucketWebsiteConfiguration, s3Bucket, s3DirectoryBucket, s3BucketObject } from '@cdktf/provider-aws';
+
+import * as fs from 'fs';
+import * as path from 'path';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -20,17 +21,32 @@ class MyStack extends TerraformStack {
       key: 'state',
     });
 
-    // Create an S3 bucket
-    const bucket = new S3Bucket(this, 'my-bucket', {
-      bucket: 'my-cdktf-demo-bucket',
+    // Define a local provisioner to sync the local directory with the S3 bucket
+    const asset = new TerraformAsset(this, 'sync-asset', {
+      path: '/Users/simon.verhoeven/Documents/workspace/typescript-journey/apps/vite-app/dist',
+      type: AssetType.DIRECTORY,
     });
 
-    // Create a text file in the S3 bucket
-    new S3BucketObject(this, 'my-bucket-object', {
-      bucket: bucket.bucket,
-      key: 'hello.txt',
-      content: 'Hello, CDK for Terraform!',
+    const myBucket = new s3Bucket.S3Bucket(this, 'my-bucket', {
+      bucket: 'cdktf-aws-demo-website-bucket',
     });
+
+    new s3BucketObject.S3BucketObject(this, 's3-bucket-object', {
+      // forEach:
+      bucket: myBucket.bucket,
+      key: asset.fileName,
+      source: asset.path, // returns a posix path
+    });
+
+    // new s3BucketWebsiteConfiguration.S3BucketWebsiteConfiguration(this, 'bucket-website', {
+    //   bucket: myBucket.bucket,
+    //   indexDocument: {
+    //     suffix: 'index.html',
+    //   },
+    //   errorDocument: {
+    //     key: '5xx.html',
+    //   },
+    // });
   }
 }
 
