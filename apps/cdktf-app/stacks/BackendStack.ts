e5b4192ceba95c3738c53d159ffd75fffa97209a
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-01 01:40:47 +0700

    lambdaRestApi constuct

--- apps/cdktf-app/stacks/BackendStack.ts
+++ apps/cdktf-app/stacks/BackendStack.ts
@@ -1,21 +1,11 @@
 import { Construct } from 'constructs';
-import { App, AssetType, Fn, S3Backend, TerraformAsset, TerraformOutput, TerraformStack } from 'cdktf';
-import {
-  lambdaFunction,
-  apiGatewayRestApi,
-  apiGatewayDeployment,
-  apiGatewayResource,
-  apiGatewayMethod,
-  apiGatewayIntegration,
-  iamRole,
-  iamRolePolicyAttachment,
-  lambdaPermission,
-} from '@cdktf/provider-aws';
+import { App, TerraformOutput } from 'cdktf';
+import { lambdaFunction, apiGatewayRestApi, apiGatewayMethod, apiGatewayIntegration } from '@cdktf/provider-aws';
 import * as path from 'path';
 import * as archive from '@cdktf/provider-archive';
 import { AwsBaseStack } from './AwsBaseStack';
-import { getConstructName } from '../utils/util';
 import { LambdaFunction } from '../constucts/LmbdaFunction';
+import { LmbdaRestApi } from '../constucts/LmbdaRestApi';
 
 export class BackendStack extends AwsBaseStack {
   constructor(scope: Construct, id: string) {
@@ -31,54 +21,12 @@ export class BackendStack extends AwsBaseStack {
       functionName: lambdaFunctionName,
     });
 
-    // console.log('lambdaPath!', lambdaPath);
-
-    // const fileName = getUniqueId(this, lambdaFunctionName);
-    // // const fileName = 'bla';
-
-    // const zipPath = path.join(process.env.INIT_CWD!, `/out/${fileName}.zip`);
-
-    // const zippedLambda = new archive.dataArchiveFile.DataArchiveFile(this, 'lambdaMyFunction', {
-    //   type: 'zip',
-    //   sourceDir: lambdaPath,
-    //   outputPath: zipPath,
-    // });
-
-    // const asset = new TerraformAsset(this, `asset`, {
-    //   path: zippedLambda.outputPath,
-    //   type: AssetType.FILE,
-    // });
-
-    const restApi = new apiGatewayRestApi.ApiGatewayRestApi(this, 'rest-api', {
-      name: getConstructName(this, 'rest-api'),
-    });
-
-    this.createApiGatewayLambdaMethod('root', restApi, restApi.rootResourceId, apiLambdaFunction.lambdaFunction);
-
-    const proxyResource = new apiGatewayResource.ApiGatewayResource(this, 'proxy-resource', {
-      restApiId: restApi.id,
-      parentId: restApi.rootResourceId,
-      pathPart: '{proxy+}',
-    });
-
-    this.createApiGatewayLambdaMethod('proxy-resource', restApi, proxyResource.id, apiLambdaFunction.lambdaFunction);
-
-    // Add Lambda permission to allow API Gateway to invoke the Lambda function
-    new lambdaPermission.LambdaPermission(this, 'api-gateway-permission', {
-      action: 'lambda:InvokeFunction',
-      functionName: apiLambdaFunction.lambdaFunction.functionName,
-      principal: 'apigateway.amazonaws.com',
-      sourceArn: `${restApi.executionArn}/*/*`,
-    });
-
-    const deployment = new apiGatewayDeployment.ApiGatewayDeployment(this, 'deployment', {
-      restApiId: restApi.id,
-      stageName: 'dev',
-      dependsOn: [proxyResource, apiLambdaFunction.lambdaFunction],
+    const lambdaRestApi = new LmbdaRestApi(this, 'lambda-rest-api', {
+      handler: apiLambdaFunction.lambdaFunction,
     });
 
     new TerraformOutput(this, 'invokeUrl', {
-      value: deployment.invokeUrl,
+      value: lambdaRestApi.url,
     });
   }
 
