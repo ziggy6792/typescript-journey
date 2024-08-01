b97e81b6b78705418a5dcdaf72a8a61535294c02
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-01 20:01:26 +0700

    get account id

--- apps/cdktf-app/main.ts
+++ apps/cdktf-app/main.ts
@@ -1,19 +1,30 @@
 import { App } from 'cdktf';
 // import { STSClient, GetCallerIdentityCommand } from 'aws-sdk';
+import { STSClient, GetCallerIdentityCommand } from '@aws-sdk/client-sts';
 import { FrontendStack } from './stacks/FrontendStack';
 import { BackendStack } from './stacks/BackendStack';
 import { PreReqStack } from './stacks/PreReqStack';
 
+const client = new STSClient({});
+
 // const client = new STSClient({});
 
-const stages = ['dev'];
+const main = async () => {
+  const command = new GetCallerIdentityCommand({});
+  const stsResponse = await client.send(command);
 
-const app = new App();
+  const stages = ['dev'];
 
-stages.forEach((stage) => {
-  const preReqStack = new PreReqStack(app, `cdktf-prereq-${stage}`, { bucketNamePrefix: `cdktf-aws-demo-bucket`, stage });
-  const backendStack = new BackendStack(app, `cdktf-backend-${stage}`, { stage, backendBucket: preReqStack.bucketName });
-  new FrontendStack(app, `cdktf-frontend-${stage}`, { stage, backendBucket: preReqStack.bucketName, apiUrl: backendStack.apiUrl });
-});
+  const app = new App();
 
-app.synth();
+  stages.forEach((stage) => {
+    const backendId = ['cdktf-aws-demo-bucket', stage, stsResponse.Account].join('-');
+    new PreReqStack(app, `cdktf-prereq-${stage}`, { backendId, stage });
+    const backendStack = new BackendStack(app, `cdktf-backend-${stage}`, { stage, backendBucket: backendId });
+    new FrontendStack(app, `cdktf-frontend-${stage}`, { stage, backendBucket: backendId, apiUrl: backendStack.apiUrl });
+  });
+
+  app.synth();
+};
+
+main();
--- apps/cdktf-app/stacks/PreReqStack.ts
+++ apps/cdktf-app/stacks/PreReqStack.ts
@@ -1,32 +1,20 @@
 import { Construct } from 'constructs';
-import { TerraformOutput, TerraformStack } from 'cdktf';
-import { dataAwsCallerIdentity, provider } from '@cdktf/provider-aws';
+import { TerraformStack } from 'cdktf';
+import { provider } from '@cdktf/provider-aws';
 import { TerraformStateBackend } from '../constucts/TerraformStateBackend';
 
 export interface PreReqStackProps {
-  bucketNamePrefix: string;
+  backendId: string;
   stage: string;
 }
 
 export class PreReqStack extends TerraformStack {
-  public readonly bucketName: string;
-
-  constructor(
-    scope: Construct,
-    id: string,
-    protected readonly props: PreReqStackProps
-  ) {
+  constructor(scope: Construct, id: string, { backendId }: PreReqStackProps) {
     super(scope, id);
-
-    // const current = new dataAwsCallerIdentity.DataAwsCallerIdentity(this, 'aws-caller-identity', {});
 
     new provider.AwsProvider(this, 'aws-provider', {
       region: 'ap-southeast-1',
     });
-
-    const backendId = [props.bucketNamePrefix, props.stage, '932244219675'].join('-');
-
-    this.bucketName = backendId;
 
     new TerraformStateBackend(this, 'terraform-sate-backend', {
       bucketName: backendId,
