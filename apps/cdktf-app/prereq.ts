e692e5c7da103f29ea11dc19bed2e88b24b5df2d
Simon Verhoeven <simon.verhoeven@noreply.github.com>
2024-08-07 05:18:47 +0700

    cleanup prereq stack

--- apps/cdktf-app/prereq.ts
+++ apps/cdktf-app/prereq.ts
@@ -1,23 +1,12 @@
 import { App } from 'cdktf';
-import { STSClient, GetCallerIdentityCommand } from '@aws-sdk/client-sts';
 import { PreReqStack } from './stacks/PreReqStack';
 import { prereqStackNames, stages } from './utils/util';
 
-const client = new STSClient({});
+const app = new App();
 
-const main = async () => {
-  const command = new GetCallerIdentityCommand({});
-  const stsResponse = await client.send(command);
+stages.forEach((stage) => {
+  const backendId = `cdktf-aws-demo-${stage}`;
+  new PreReqStack(app, prereqStackNames[stage], { backendName: backendId });
+});
 
-  const app = new App();
-
-  stages.forEach((stage) => {
-    const backendBucket = ['cdktf-aws-demo', stage, stsResponse.Account].join('-');
-    const dynamodbTable = ['cdktf-aws-demo', stage].join('-');
-    new PreReqStack(app, prereqStackNames[stage], { bucket: backendBucket, dynamodbTable });
-  });
-
-  app.synth();
-};
-
-main();
+app.synth();
--- apps/cdktf-app/stacks/PreReqStack.ts
+++ apps/cdktf-app/stacks/PreReqStack.ts
@@ -1,32 +1,34 @@
 import { Construct } from 'constructs';
 import { TerraformOutput, TerraformStack } from 'cdktf';
 import { provider } from '@cdktf/provider-aws';
+import { DataAwsCallerIdentity } from '@cdktf/provider-aws/lib/data-aws-caller-identity';
 import { S3DynamodbRemoteBackend } from '../.gen/modules/s3-dynamodb-remote-backend';
 
 export interface PreReqStackProps {
-  bucket: string;
-  dynamodbTable: string;
+  backendName: string;
 }
 
 export class PreReqStack extends TerraformStack {
-  constructor(scope: Construct, id: string, { bucket, dynamodbTable }: PreReqStackProps) {
+  constructor(scope: Construct, id: string, { backendName }: PreReqStackProps) {
     super(scope, id);
+
+    const currentAccount = new DataAwsCallerIdentity(this, 'current-account', {});
 
     new provider.AwsProvider(this, 'aws-provider', {
       region: 'ap-southeast-1',
     });
 
-    new S3DynamodbRemoteBackend(this, 's3-dynamodb-remote-backend', {
-      bucket,
-      dynamodbTable,
+    const bakend = new S3DynamodbRemoteBackend(this, 's3-dynamodb-remote-backend', {
+      bucket: `${backendName}-${currentAccount.accountId}`,
+      dynamodbTable: backendName,
     });
 
     new TerraformOutput(this, 'bucket', {
-      value: bucket,
+      value: bakend.bucket,
     });
 
     new TerraformOutput(this, 'dynamodbTable', {
-      value: dynamodbTable,
+      value: bakend.dynamodbTable,
     });
   }
 }
